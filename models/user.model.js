bf4f5da61c5481680e3c6998fb97feed429b131a
lind1125 <danieljlinden@gmail.com>
2020-12-23 20:44:09 -0600

    updated user model

--- models/user.model.js
+++ models/user.model.js
@@ -6,6 +6,7 @@ const User = mongoose.model(
        username: String,
        email: String,
        password: String,
+       location: String,
        dogs: [
            {
                type: mongoose.Schema.Types.ObjectId,
