a89951926c9ef66d6afa3479748eb067e2ef32cb
Monica <monica.parrillo@gmail.com>
2020-12-27 13:44:58 -0600

    first pass at delete route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,16 +2,13 @@ const config = require('../config/auth.config')
 const db = require('../models/index')
 // Access to our db through User
 const User = db.user
+const Dog = db.dog
 
 // exports.allAccess = (req,res) => {
 //     res.status(200).send("public content")
 // }
 
-// exports.userBoard = (req,res) => {
-//
-//
-//     res.status(200).send("User content")
-// }
+
 
 
 // Get request to /profile, get's user information and their dogs
@@ -27,4 +24,27 @@ exports.getProfile = (req,res) => {
       console.log(user)
       return res.status(200).send(user)
     })
+}
+
+// Delete a user's profile and it's associated dogs from database.
+exports.deleteProfile = (req,res) => {
+  User.findOne({
+    _id:req.userId
+  }).then((user,err)=>{
+    if(err){
+      return res.status(500).send({message: err.message})
+    }
+    console.log(user)
+    // need to go through all the user's dogs and delete them
+    // while(user.dogs){
+    //   // TODO test this syntax
+    //   let dog = user.dogs.pop()
+    //   Dog.remove()
+    // }
+    User.deleteOne({_id:req.userId}).then(result=>{
+      console.log(result)
+      // should we redirect here? TODO
+      return res.status(200).send({message: "deleted user"})
+    })
+  })
 }
